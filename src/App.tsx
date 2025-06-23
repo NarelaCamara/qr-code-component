1bc1524fae96989d338e8911211bfa1a0315c57a
RoxanaCamara <roxana.camara07@gmail.com>
2025-06-23 01:38:37 -0300

    feat: react vite + ts

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import viteLogo from "./assets/image-qr-code.png";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <div className="card">
+        <img src={viteLogo} className="image" alt="qr" />
+        <h2>Improve your front-end skills by building projects</h2>
+        <p>
+          Scan the QR code to visit Frontend Mentor and take your coding skills
+          to the next level
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
